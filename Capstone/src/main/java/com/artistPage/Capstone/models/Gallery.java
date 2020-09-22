c2f6bca8cfabb6e001c0a389fb7b25b05c5bb735
aKMiller <akathryn.miller@gmail.com>
2020-09-22 11:11:04 -0500

    couple of errors/added @OneTOMany & @ManyToOne

--- Capstone/src/main/java/com/artistPage/Capstone/models/Gallery.java
+++ Capstone/src/main/java/com/artistPage/Capstone/models/Gallery.java
@@ -1,12 +1,16 @@
 package com.artistPage.Capstone.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 public class Gallery extends AbstractEntity {
 
+    @ManyToOne
+    @JoinColumn
     private List<Image> images = new ArrayList<>();
 
     public Gallery(List<Image> images){
