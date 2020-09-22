0c9d46e5dd6aaa51b68f24e7fbd38b42dd7e7f26
aKMiller <akathryn.miller@gmail.com>
2020-09-22 10:52:12 -0500

    added list images to gallery/constructor/getters&setters

--- Capstone/src/main/java/com/artistPage/Capstone/models/Gallery.java
+++ Capstone/src/main/java/com/artistPage/Capstone/models/Gallery.java
@@ -1,11 +1,24 @@
 package com.artistPage.Capstone.models;
 
 import javax.persistence.Entity;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Gallery extends AbstractEntity {
 
+    private List<Image> images = new ArrayList<>();
 
+    public Gallery(List<Image> images){
+        super();
+        this.images = images;
+    }
 
+    public List<Image> getImages() {
+        return images;
+    }
 
+    public void setImages(List<Image> images) {
+        this.images = images;
+    }
 }
