b97c569b5dab69fb951ef48c14ce12577e4a6860
aKMiller <akathryn.miller@gmail.com>
2020-09-22 11:30:55 -0500

    Deleted Gallery class/ not needed.

--- Capstone/src/main/java/com/artistPage/Capstone/models/Image.java
+++ Capstone/src/main/java/com/artistPage/Capstone/models/Image.java
@@ -1,13 +1,10 @@
 package com.artistPage.Capstone.models;
 
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
 
 @Entity
 public class Image extends AbstractEntity {
 
-    @ManyToOne
-    private Gallery gallery;
 
     public Image(){
 
