bdf9ebc755909738775a07d86dc058da7720ae0f
aKMiller <akathryn.miller@gmail.com>
2020-09-24 11:41:34 -0500

    Added ImageRepository to CartController

--- Capstone/src/main/java/com/artistPage/Capstone/controllers/CartController.java
+++ Capstone/src/main/java/com/artistPage/Capstone/controllers/CartController.java
@@ -1,5 +1,7 @@
 package com.artistPage.Capstone.controllers;
 
+import com.artistPage.Capstone.models.data.ImageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,4 +14,8 @@ public class CartController {
     public String shoppingCart(Model model){
         return "cart";
     }
+
+    @Autowired
+    private ImageRepository imageRepository;
+
 }
