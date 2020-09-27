3fadc7d67dd6da7764ca0a45e61c53835b708733
aKMiller <akathryn.miller@gmail.com>
2020-09-27 10:51:22 -0500

    Muted listImagesByColumnAndPrice method and changed my list to a table in cart.html

--- Capstone/src/main/java/com/artistPage/Capstone/controllers/CartController.java
+++ Capstone/src/main/java/com/artistPage/Capstone/controllers/CartController.java
@@ -6,16 +6,35 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.HashMap;
+
 @Controller
 @RequestMapping("cart")
 public class CartController {
 
     @RequestMapping("")
-    public String shoppingCart(Model model){
+    public String cart(Model model) {
         return "cart";
     }
 
     @Autowired
     private ImageRepository imageRepository;
 
+    static HashMap<String, String> columnImages = new HashMap<>();
+
+    public CartController() {
+
+        columnImages.put("image", "Image");
+    }
+
+//    @RequestMapping(value = "images")
+//    public String listImagesByColumnAndPrice(Model model, @RequestParam String column, @RequestParam String price) {
+//        Iterable<Image> images;
+//        if (column.equals("images")) {
+//            images = imageRepository.findAll();
+//            model.addAttribute("title", "All Images" + price);
+//        }
+//        model.addAttribute("images", images);
+//        return "cart";
+//    }
 }
